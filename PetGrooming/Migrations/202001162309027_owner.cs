7be8acfc74b1681fefd93f4f1fed536ba0c1f4ad
navjot <navjotkaurgill05@gmail.com>
2020-01-23 16:31:04 -0500

    Add project files.

---
+++ PetGrooming/Migrations/202001162309027_owner.cs
@@ -0,0 +1,32 @@
+﻿namespace PetGrooming.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class owner : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Owners",
+                c => new
+                    {
+                        OwnerId = c.String(nullable: false, maxLength: 128),
+                        OwnerFName = c.String(),
+                        OwnerLName = c.String(),
+                        OwnerPhone = c.Int(nullable: false),
+                        OwnerEmail = c.String(),
+                        OwnerAddress = c.String(),
+                    })
+                .PrimaryKey(t => t.OwnerId);
+            
+            AddColumn("dbo.Pets", "DOB", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Pets", "DOB");
+            DropTable("dbo.Owners");
+        }
+    }
+}
